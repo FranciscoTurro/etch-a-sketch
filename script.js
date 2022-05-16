afdbdfd14e9b062e272abee3f30210bf1f2e19bd
FranciscoTurro <franciscogtrro@outlook.com.ar>
2022-05-16 19:59:46 -0300

    as

---
+++ script.js
@@ -0,0 +1,30 @@
+const container = document.getElementById("container");
+let rows = document.getElementsByClassName("gridRow");
+let cells = document.getElementsByClassName("cell");
+
+function defaultGrid(rows,columns) {
+    makeRows(rows);
+    makeColumns(columns);
+}
+
+function makeRows(rowNum) {
+    for (r = 0; r < rowNum; r++) {
+        let row = document.createElement("div");
+        container.appendChild(row).className = "gridRow";
+    };
+};
+
+function makeColumns(cellNum) {
+    for (i = 0; i < rows.length; i++) {
+        for (j = 0; j < cellNum; j++) {
+            let newCell = document.createElement("div");
+            rows[j].appendChild(newCell).className = "cell";
+        };
+    };
+};
+
+defaultGrid(16,16);
+
+container.addEventListener("mouseover",function(event) {
+event.target.style.backgroundColor='black';
+})
