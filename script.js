00410f4f217d7fb3c5e452ea39c70cdee25f8537
FranciscoTurro <franciscogtrro@outlook.com.ar>
2022-05-18 12:12:06 -0300

    Basic functionality done

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 const container = document.getElementById("container");
-let rows = document.getElementsByClassName("gridRow");
-let cells = document.getElementsByClassName("cell");
+let cell;
 
-function defaultGrid(rows,columns) {
-    makeRows(rows);
-    makeColumns(columns);
+function makeCell(cellNum) {
+    for (let i=0;i<cellNum;i++) {
+        cell = document.createElement('div');
+        cell.classList.add('cell');
+        container.appendChild(cell);
+    }
 }
 
-function makeRows(rowNum) {
-    for (r = 0; r < rowNum; r++) {
-        let row = document.createElement("div");
-        container.appendChild(row).className = "gridRow";
-    };
-};
-//fuck
-function makeColumns(cellNum) {
-    for (i = 0; i < rows.length; i++) {
-        for (j = 0; j < cellNum; j++) {
-            let newCell = document.createElement("div");
-            rows[j].appendChild(newCell).className = "cell";
-        };
-    };
-};
+function makeGrid(numRow,numCol) {
+    container.style.setProperty('--numRows',numRow);
+    container.style.setProperty('--numCols',numCol);
+    for (let i=0;i<numRow;i++) {
+        makeCell(numCol);
+    }
+}
 
-defaultGrid(16,16);
+container.addEventListener('mouseover',(e) => {
+    if (e.target.matches("div.cell")) {
+        changeColor(e.target);
+    }
+});
 
-container.addEventListener("mouseover",function(event) {
-event.target.style.backgroundColor='black';
-})
+function changeColor(target) {
+    target.style.backgroundColor = "black";
+}
+
+makeGrid(16,16)
