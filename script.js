4bfa6b0e6bcc25ba2f62e147fdf7965ff36210f5
FranciscoTurro <franciscogtrro@outlook.com.ar>
2022-05-19 10:43:45 -0300

    Finished, css is ironic bad

--- script.js
+++ script.js
@@ -17,31 +17,53 @@ function makeGrid(numRow, numCol) {
   }
 }
 
-let mouseDown = false;
-document.body.onmousedown = () => (mouseDown = true);
-document.body.onmouseup = () => (mouseDown = false);
+let RandomEvent = false;
+
+const randomPick = document.getElementById("random");
+randomPick.addEventListener("click",()=> {
+  RandomEvent = true;
+})
 
 container.addEventListener("mouseover", (e) => {
-  if (e.target.matches("div.cell") && mouseDown) {
-    changeColor(e.target);
+  if (RandomEvent == false) {
+    if (e.target.matches("div.cell")) {
+      changeColor(e.target);
+    }
   }
-});
+  else if (RandomEvent == true) {
+    if (e.target.matches("div.cell")) {
+      changeColorRandom(e.target);
+  }
+}});
 
 function changeColor(target) {
   target.style.backgroundColor = "black";
 }
 
+function changeColorRandom(target) {
+  let RandomR = Math.floor(Math.random() * 256);
+  let RandomG = Math.floor(Math.random() * 256);
+  let RandomB = Math.floor(Math.random() * 256);
+  target.style.backgroundColor = `rgb(${RandomR}, ${RandomG}, ${RandomB})`
+}
+
 const clear = document.getElementById("clear");
-clear.addEventListener('click',() => {
+clear.addEventListener("click", () => {
   document.location.reload();
 });
 
 const choose = document.getElementById("choose");
-choose.addEventListener("click",() => {
+choose.addEventListener("click", () => {
   let n1 = prompt("First number");
   let n2 = prompt("Second number");
+  container.innerHTML = "";
   makeGrid(n1, n2);
+});
+
+const backinblack = document.getElementById("black-color");
+backinblack.addEventListener("click",()=> {
+  RandomEvent = false;
 })
 
-makeGrid(16, 16);
 
+makeGrid(16, 16);
