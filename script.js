7a5cdb7693e70ab47d01b161071582bf4c8b440a
FranciscoTurro <franciscogtrro@outlook.com.ar>
2022-05-18 21:27:51 -0300

    Added clear button, rehaul css later

--- script.js
+++ script.js
@@ -2,29 +2,40 @@ const container = document.getElementById("container");
 let cell;
 
 function makeCell(cellNum) {
-    for (let i=0;i<cellNum;i++) {
-        cell = document.createElement('div');
-        cell.classList.add('cell');
-        container.appendChild(cell);
-    }
+  for (let i = 0; i < cellNum; i++) {
+    cell = document.createElement("div");
+    cell.classList.add("cell");
+    container.appendChild(cell);
+  }
 }
 
-function makeGrid(numRow,numCol) {
-    container.style.setProperty('--numRows',numRow);
-    container.style.setProperty('--numCols',numCol);
-    for (let i=0;i<numRow;i++) {
-        makeCell(numCol);
-    }
+function makeGrid(numRow, numCol) {
+  container.style.setProperty("--numRows", numRow);
+  container.style.setProperty("--numCols", numCol);
+  for (let i = 0; i < numRow; i++) {
+    makeCell(numCol);
+  }
 }
 
-container.addEventListener('mouseover',(e) => {
-    if (e.target.matches("div.cell")) {
-        changeColor(e.target);
-    }
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+container.addEventListener("mouseover", (e) => {
+  if (e.target.matches("div.cell") && mouseDown) {
+    changeColor(e.target);
+  }
 });
 
 function changeColor(target) {
-    target.style.backgroundColor = "black";
+  target.style.backgroundColor = "black";
 }
 
-makeGrid(16,16);
+const clear = document.getElementById("clear");
+clear.addEventListener('click',() => {
+  document.location.reload();
+});
+
+
+makeGrid(16, 16);
+
